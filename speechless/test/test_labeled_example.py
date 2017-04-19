74a061c6bf54f5546f58f27ef22195bac41751b3
Julius Kunze <juliuskunze@gmail.com>
2017-04-19 10:32:13 +0200

    added back tests

---
+++ speechless/test/test_labeled_example.py
@@ -0,0 +1,21 @@
+import librosa
+import numpy as np
+from unittest import TestCase
+
+from speechless.english_corpus import LibriSpeechCorpus
+from speechless.labeled_example import SpectrogramType, SpectrogramFrequencyScale
+from speechless.tools import home_directory
+
+corpus = LibriSpeechCorpus(home_directory() / "speechless-data" / "corpus" / "English", corpus_name="dev-clean")
+
+
+class LabeledExampleTest(TestCase):
+    def test(self):
+        example = corpus.examples[0]
+        mel_power_spectrogram = librosa.feature.melspectrogram(
+            y=example.raw_audio(), n_fft=example.fourier_window_length, hop_length=example.hop_length,
+            sr=example.sample_rate)
+
+        self.assertTrue(np.array_equal(mel_power_spectrogram,
+                                       example.spectrogram(type=SpectrogramType.power,
+                                                           frequency_scale=SpectrogramFrequencyScale.mel)))
